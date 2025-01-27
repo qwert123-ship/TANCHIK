02a0930be18026a2bc020bf2758deb661f738b0c
ЧЕМОДАН ЕБУЧИЙ <75309755+qwert123-ship@users.noreply.github.com>
2025-01-27 18:32:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -54,12 +54,13 @@ while running:
         last_direction = [0, 1]
         moved = True
 
-    if moved and keys[pygame.K_f]:
+    grid[tank_pos[0], tank_pos[1]] = 1
+
+    # Проверка на нажатие клавиши "F" для выстрела
+    if keys[pygame.K_f]:
         # Создание снаряда, который будет двигаться в последнем направлении
         bullet_pos = tank_pos.copy()
         bullets.append([bullet_pos, last_direction])
-
-    grid[tank_pos[0], tank_pos[1]] = 1
 
     # Обновление позиции снарядов
     for bullet in bullets:
@@ -85,6 +86,7 @@ while running:
                          (bullet[0][1] * CELL_SIZE, bullet[0][0] * CELL_SIZE, CELL_SIZE // 4, CELL_SIZE // 4))
 
     pygame.display.flip()
-    pygame.time.delay(180)
+    pygame.time.delay(100)
 
 pygame.quit()
+
